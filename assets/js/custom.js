0c9f6af42f4481d8c815169064d72ed5b59d335f
Rai rasta <setiawanpanjirai@gmail.com>
2022-04-10 15:29:37 +0700

    update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,37 @@
 
+$(document).ready(function() {
+    clockUpdate();
+    setInterval(clockUpdate, 1000);
+  })
+  
+  function clockUpdate() {
+    var date = new Date();
+    $('.digital-clock').css({'color': '#fff', 'text-shadow': '0 0 6px #ff0'});
+    function addZero(x) {
+      if (x < 10) {
+        return x = '0' + x;
+      } else {
+        return x;
+      }
+    }
+  
+    function twelveHour(x) {
+      if (x > 12) {
+        return x = x - 12;
+      } else if (x == 0) {
+        return x = 12;
+      } else {
+        return x;
+      }
+    }
+  
+    var h = addZero(twelveHour(date.getHours()));
+    var m = addZero(date.getMinutes());
+    var s = addZero(date.getSeconds());
+  
+    $('.digital-clock').text(h + ':' + m + ':' + s)
+  }
+  
 	var date = '01/01/1997';
  
 	if(date === ""){
